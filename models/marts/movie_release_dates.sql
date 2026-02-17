4c073bf10e77588ea94f58a7cc95108263866d51
ivansiase <ivansiase@yahoo.com>
2026-02-17 23:10:46 +0800

    Added movie release dates marts that aligns to the uploaded seed csv file

---
+++ models/marts/movie_release_dates.sql
@@ -0,0 +1,18 @@
+{{ config(materialized = 'table') }}
+
+WITH fct_ratings AS (
+    SELECT * FROM {{ ref('fct_ratings') }}
+),
+seed_dates AS (
+    SELECT * FROM {{ ref('Release_dates') }}
+)
+
+SELECT 
+    f.*,
+    CASE
+        WHEN d.release_date IS NULL THEN 'unknown'
+        ELSE 'known'
+    END AS release_info_available
+FROM fct_ratings f
+LEFT JOIN seed_dates d
+ON f.movie_id = d.movie_id
